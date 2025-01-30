ae0c773937def6447f36c86a2187edfa0daa2bb1
Faizan Ali <alif32398@gmail.com>
2025-01-30 12:01:11 +0500

    Blog and Post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
       <Testomonials />
       <CompanyNameSlider />
       <TailoredForEverySpecialty/>
-      {/* <PostArticles/> */}
+      <PostArticles/>
       <ImmediateAssistance />
     </>
   );
